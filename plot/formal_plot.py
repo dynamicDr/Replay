b8f6bea94d18c61bf900e1406106f4495e7608f0
dty <1102310490@qq.com>
2023-03-21 20:17:20 +0800

    1

--- plot/formal_plot.py
+++ plot/formal_plot.py
@@ -42,40 +42,40 @@ for file in os.listdir('plot/formal'):
             data[alg] = {}
         data[alg][metric] = df
 
-# # 绘制 reward 图
-# plt.figure(figsize=(12, 8))
-# sns.set_style("whitegrid")
-# palette = sns.color_palette("deep", len(data))
-# for i, alg in enumerate(data):
-#     df = data[alg]['reward']
-#     df['Smoothed'] = df['Value'].rolling(window=1500, min_periods=1).mean()
-#     df['Std'] = df['Value'].rolling(window=1500, min_periods=1).std()/10
-#     plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
-#     plt.fill_between(df['Step'][100:], df['Smoothed'][100:]-df['Std'][100:], df['Smoothed'][100:]+df['Std'][100:], alpha=0.1, color=palette[i])
-# plt.xlabel('Episode')
-# plt.ylabel('Return')
-# plt.title('Return Comparison')
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig('plot/reward.jpg')
-#
-# plt.clf()
-# # 绘制 reward 图
-# plt.figure(figsize=(12, 8))
-# sns.set_style("whitegrid")
-# palette = sns.color_palette("deep", len(data))
-# for i, alg in enumerate(data):
-#     df = data[alg]['goal']
-#     df['Smoothed'] = df['Value'].rolling(window=1500, min_periods=1).mean()
-#     df['Std'] = df['Value'].rolling(window=1500, min_periods=1).std()/10
-#     plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
-#     plt.fill_between(df['Step'][100:], df['Smoothed'][100:]-df['Std'][100:], df['Smoothed'][100:]+df['Std'][100:], alpha=0.1, color=palette[i])
-# plt.xlabel('Episode')
-# plt.ylabel('Goal Scored')
-# plt.title('Goal Scored Comparison')
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig('plot/goal.jpg')
+# 绘制 reward 图
+plt.figure(figsize=(12, 8))
+sns.set_style("whitegrid")
+palette = sns.color_palette("deep", len(data))
+for i, alg in enumerate(data):
+    df = data[alg]['reward']
+    df['Smoothed'] = df['Value'].rolling(window=1500, min_periods=1).mean()
+    df['Std'] = df['Value'].rolling(window=1500, min_periods=1).std()/10
+    plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
+    plt.fill_between(df['Step'][100:], df['Smoothed'][100:]-df['Std'][100:], df['Smoothed'][100:]+df['Std'][100:], alpha=0.1, color=palette[i])
+plt.xlabel('Episode')
+plt.ylabel('Average Episode Reward')
+plt.title('Average Episode Reward Comparison')
+plt.legend()
+plt.tight_layout()
+plt.savefig('plot/reward.jpg')
+
+plt.clf()
+# 绘制 reward 图
+plt.figure(figsize=(12, 8))
+sns.set_style("whitegrid")
+palette = sns.color_palette("deep", len(data))
+for i, alg in enumerate(data):
+    df = data[alg]['goal']
+    df['Smoothed'] = df['Value'].rolling(window=1500, min_periods=1).mean()
+    df['Std'] = df['Value'].rolling(window=1500, min_periods=1).std()/10
+    plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
+    plt.fill_between(df['Step'][100:], df['Smoothed'][100:]-df['Std'][100:], df['Smoothed'][100:]+df['Std'][100:], alpha=0.1, color=palette[i])
+plt.xlabel('Episode')
+plt.ylabel('Goal Scored')
+plt.title('Goal Scored Comparison')
+plt.legend()
+plt.tight_layout()
+plt.savefig('plot/goal.jpg')
 
 plt.clf()
 # 绘制 index 图
@@ -85,21 +85,16 @@ palette = sns.color_palette("deep", len(data))
 plt.xlim(0, 10000)
 plt.ylim(10000, 60000)
 
-for i, alg in enumerate(data):
-    df = data[alg]['index']
 
-    if alg == "Vanilla ER":
-        pass
-    elif alg == "PER":
-        df['Smoothed'] = df['Value'].rolling(window=200, min_periods=1).mean()
-        plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
-    elif alg == "CER":
-        df['Smoothed'] = df['Value'].rolling(window=1000, min_periods=1).mean()
-        plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
-    elif alg == "APER":
-        df['Smoothed'] = df['Value'].rolling(window=200, min_periods=1).mean()
-        if i<5000:
-            plt.plot(df['Step'][100:], df['Smoothed'][100:], label=alg, color=palette[i])
+
+for i, alg in enumerate(data):
+    df = data[alg]['reward']
+    m = 77.5
+    print(alg, m)
+    for j in range(6000,len(df)):
+        if df["Value"].iloc[j-100:j+100].mean()>m:
+            print(j)
+            break
 
     # plt.fill_between(df['Step'][100:], df['Smoothed'][100:]-df['Std'][100:], df['Smoothed'][100:]+df['Std'][100:], alpha=0.1, color=palette[i])
 plt.xlabel('Episode')
@@ -108,3 +103,4 @@ plt.title('The step difference between a transaction is add and sampled')
 plt.legend()
 plt.tight_layout()
 plt.savefig('plot/index.jpg')
+print("done")
